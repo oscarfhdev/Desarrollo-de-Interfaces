d336601afa7ebec3dc3bf6ee820d8cc0cafdcc38
oscarfhdev <oscarfh.dev@gmail.com>
2025-10-22 23:17:08 +0200

    UPDATED TEMA2/JAVAFX/FrigorificoInteligente --> finished homepage

--- TEMA 2/JAVAFX/FrigorificoInteligente/src/frigorifico_inteligente/controlador/PantallaPrincipalControlador.java
+++ TEMA 2/JAVAFX/FrigorificoInteligente/src/frigorifico_inteligente/controlador/PantallaPrincipalControlador.java
@@ -10,6 +10,8 @@ import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Duration;
 
 
@@ -19,28 +21,44 @@ public class PantallaPrincipalControlador implements Initializable {
     private Label labelHora;
     @FXML
     private Label labelFecha;
+    @FXML
+    private Slider slideTemperatura;
+    @FXML
+    private Label labelTemperatura;
+    @FXML
+    private Label labelPorcentajeCalidad;
 
-    /**
-     * Initializes the controller class.
-     */
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         inicializarFechayHora();
+        asociarLabelSlide();
     }    
+
+    private void asociarLabelSlide() {
+        labelTemperatura.textProperty().bind(
+    slideTemperatura.valueProperty().asString("%.1f " + "º")
+        );
+    }
     
     public void inicializarFechayHora(){
-        // Formato de hora y fecha
+        // Formato de fecha y hora
         DateTimeFormatter formatoHora = DateTimeFormatter.ofPattern("HH:mm:ss");
         DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        // Timeline que se ejecuta cada segundo
+        // Evento que se ejecuta cada segundo
         Timeline reloj = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
-            LocalDateTime ahora = LocalDateTime.now();
-            labelHora.setText(formatoHora.format(ahora));
-            labelFecha.setText(formatoFecha.format(ahora));
+            LocalDateTime dateTimeActual = LocalDateTime.now();
+            labelHora.setText(formatoHora.format(dateTimeActual));
+            labelFecha.setText(formatoFecha.format(dateTimeActual));
         }));
         reloj.setCycleCount(Timeline.INDEFINITE);
         reloj.play();
+    }
+
+    @FXML
+    private void apagarFrigorifico(MouseEvent event) {
+        System.exit(0);
     }
 
     
