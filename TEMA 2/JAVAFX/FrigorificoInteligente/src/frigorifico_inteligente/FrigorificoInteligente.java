aa7e2deb73268210820c5c31bdfcd07138f6a41c
oscarfhdev <oscarfh.dev@gmail.com>
2025-10-22 21:16:51 +0200

    CREATED TEMA2/JAVAFX/FrigorificoInteligente --> initialized project

---
+++ TEMA 2/JAVAFX/FrigorificoInteligente/src/frigorifico_inteligente/FrigorificoInteligente.java
@@ -0,0 +1,38 @@
+package frigorifico_inteligente;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class FrigorificoInteligente extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/frigorifico_inteligente/vista/pantallaPrincipal.fxml"));
+            
+            // Cargo el scene
+            
+            Scene scene = new Scene(root);
+            
+            primaryStage.setTitle("Frigorifico Inteligente");
+            // Seteo la scene y la muestro
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
