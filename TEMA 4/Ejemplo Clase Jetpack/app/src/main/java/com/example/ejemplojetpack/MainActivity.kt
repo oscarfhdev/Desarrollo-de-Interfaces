cdf51b52317c87e3d0c1db22c53439a0bd998c70
oscarfhdev <oscarfh.dev@gmail.com>
2026-04-15 10:20:03 +0200

    UPDATED MainActivity.kt --> new class examples

--- TEMA 4/Ejemplo Clase Jetpack/app/src/main/java/com/example/ejemplojetpack/MainActivity.kt
+++ TEMA 4/Ejemplo Clase Jetpack/app/src/main/java/com/example/ejemplojetpack/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
@@ -23,6 +25,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ejemplojetpack.ui.theme.EjemploJetpackTheme
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +50,23 @@ class MainActivity : ComponentActivity() {
 fun previsualizacion(){
     // crearColumna()
     //crearFila()
-    crearMuchasFilas()
+    //crearMuchasFilas()
+    //val mensaje = textos("Pepe", "Jetpack")
+    //mensajeClase(mensaje)
+    //crearFilaClase(mensaje)
+    /*val arrayMensajes: List<textos> = listOf<textos>(
+        textos("Pepe", "Jetpack"),
+        textos("Juan", "Jetpack"),
+        textos("Pedro", "Jetpack"),
+        textos("Maria", "Jetpack"),
+        textos("Jose", "Jetpack"),
+        textos("Ana", "Jetpack"),
+        textos("Luis", "Jetpack")
+    )*/
+
+    //crearColumnaClase(arrayMensajes)
+    extraerVariable()
+
 }
 
 @Composable
@@ -120,4 +145,57 @@ fun mostrarSuma(num1: Int, num2: Int){
 
 fun sumar(num1: Int, num2: Int): Int {
     return num1 + num2
+}
+
+/*@Composable
+fun mensajeClase(mensaje: textos){
+    Text("Hola " + mensaje.texto1 + " bienvenido a " + mensaje.texto2, modifier = Modifier.padding(30.dp))
+}*/
+
+@Composable
+fun crearFilaClase(mensaje: textos){
+    var estadoLineas by remember { mutableStateOf(false) }
+
+    Row(modifier = Modifier.padding(36.dp).clickable{
+        estadoLineas = !estadoLineas
+    }) {
+
+        Image(
+            painter = painterResource(id = R.drawable.icono),
+            contentDescription = "Imagen 1",
+            modifier = Modifier.size(50.dp)
+        )
+        mensajeClase(mensaje, estadoLineas)
+    }
+}
+
+@Composable
+fun crearColumnaClase(mensaje: List<textos>){
+    LazyColumn() {
+        items(mensaje.size){
+            crearFilaClase(mensaje[it])
+        }
+    }
+}
+
+@Composable
+fun mensajeClase(mensaje: textos, estado: Boolean) {
+    Text("Hola " + mensaje.texto1 + " bienvenido a " + mensaje.texto2 + " aaaaaaa aaaaaaaaaaaaaaaaaaaaaaa aaaaaa aaaaaaaaaaa", modifier = Modifier.padding(30.dp), maxLines = if (estado) Int.MAX_VALUE else 1)
+}
+
+@Composable
+fun extraerVariable(){
+    var numero by remember { mutableStateOf(0) }
+
+    Column(modifier = Modifier.padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text("Número 1")
+        OutlinedTextField(value = numero.toString(),
+            onValueChange = {newValue -> if (newValue.all { it.isDigit() }) numero = newValue.toInt()
+            }
+        )
+        Text("el número es: $numero" )
+    }
+
 }
